af614c827ea7070848b7302cfb5a8a66eefa1a59
l.ropot <l.ropot@tinkoff.ru>
2023-09-07 14:58:27 +0300

    add first test

---
+++ python_test/first_func.py
@@ -0,0 +1,3 @@
+def func(a: int, b: int):
+    result = a + b
+    return result
---
+++ python_test/test_func.py
@@ -0,0 +1,6 @@
+from first_func import func
+
+
+def test():
+    assert func(5, 8) == 13
+    assert func(5, 9) == 14
