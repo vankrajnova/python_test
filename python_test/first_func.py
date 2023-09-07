75d52f5abfb2739d6461ad353da39398dcbf57e2
l.ropot <l.ropot@tinkoff.ru>
2023-09-07 17:52:50 +0300

    Add test to Url

--- python_test/first_func.py
+++ python_test/first_func.py
@@ -1,3 +1,11 @@
+import requests
+
+
 def func(a: int, b: int):
     result = a + b
     return result
+
+
+def func_req(url: str):
+    url_text = requests.get(url)
+    return url_text
--- python_test/test_func.py
+++ python_test/test_func.py
@@ -1,6 +1,12 @@
 from first_func import func
+from first_func import func_req
 
 
 def test():
     assert func(5, 8) == 13
     assert func(5, 9) == 14
+
+
+def test_url():
+    url_address = 'https://scotch.io'
+    assert func_req(url_address).status_code == 200
